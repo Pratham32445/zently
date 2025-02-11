76390992b5743ffe9f072af48d1bc521a870c2c8
Pratham mehta <mehtapratham49@gmail.com>
2025-02-11 13:13:41 +0530

    added netauth

---
+++ app/api/[...nextauth]/options.ts
@@ -0,0 +1,24 @@
+import { AuthOptions } from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+
+export const authOptions: AuthOptions = {
+  providers: [
+    CredentialsProvider({
+      name: "Welcome Back",
+      credentials: {
+        email: {
+          type: "email",
+          placeholder: "Enter your email",
+        },
+        password: {
+          type: "password",
+          placeholder: "Enter your password",
+        },
+      },
+      authorize(credentials, req) {
+        console.log(credentials,req);
+        return null;
+      },
+    }),
+  ],
+};
