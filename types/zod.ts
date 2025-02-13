1b02f42e44286333ff094ac2db094e14669ac906
Pratham mehta <mehtapratham49@gmail.com>
2025-02-13 18:41:27 +0530

    created project

--- types/zod.ts
+++
@@ -1,7 +0,0 @@
-import { z } from "zod";
-
-export const signUpSchema = z.object({
-  userName: z.string().min(4, "username must be more than 4 characters"),
-  email: z.string().email("Please enter a valid email"),
-  password: z.string().min(6, "Please enter a 6 digit password"),
-});
